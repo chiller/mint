9105197d1e37a546f919836d7a2774f1ba378326
chiller <egalaczi_business@yahoo.com>
2013-09-17 19:35:17 +0200

    simple rest api - no db

--- app.js
+++ app.js
@@ -5,7 +5,8 @@
 
 var express = require('express'),
   routes = require('./routes'),
-  socket = require('./routes/socket.js');
+  socket = require('./routes/socket.js'),
+  api = require('./routes/api');
 
 var app = module.exports = express.createServer();
 
@@ -13,6 +14,7 @@ var app = module.exports = express.createServer();
 var io = require('socket.io').listen(app);
 
 // Configuration
+
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -23,6 +25,7 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.static(__dirname + '/public'));
+  app.use(express.logger());
   app.use(app.router);
 });
 
@@ -35,11 +38,18 @@ app.configure('production', function(){
 });
 
 // Routes
+app.get('/api/docs', api.docs);
+app.get('/api/docs/:id', api.doc);
+app.post('/api/docs', api.adddoc);
+app.delete('/api/docs/:id', api.deletedoc);
+
+
 app.get('/editor', function(req, res){
   res.render('editor', {
     title: 'About'
   });
 });
+app.get('/playground', function(req, res){res.render('playground');});
 app.get('/', routes.index);
 app.get('/partials/:name', routes.partials);
 

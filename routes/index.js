3fc8dacb05b84c1288c72929eef265cc8f978bc7
chiller <egalaczi_business@yahoo.com>
2013-09-18 16:39:43 +0200

    entity api

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
-var Api = require('./api');
+var DocumentApi = require('./api/docs.js');
+var EntityApi = require('./api/entities.js');
 module.exports = exports = function(app, db) {
 // Routes
-    api = new Api(db);
+    
+    //document api
+    api = new DocumentApi(db);
     app.get('/api/docs', api.docs);
     app.get('/api/docs/:id', api.doc);
     app.post('/api/docs', api.adddoc);
     app.put('/api/docs/:id', api.updatedoc);
     app.delete('/api/docs/:id', api.deletedoc);
+    //entity api
+    entities = new EntityApi(db);
+    app.get('/api/entities', entities.getAll);
+    app.get('/api/entities/:id', entities.getOne);
+    app.post('/api/entities', entities.create);
+    app.put('/api/entities/:id', entities.update);
+    app.delete('/api/entities/:id', entities.delete);
 
 
+    //pages
     app.get('/editor', function(req, res){
       res.render('editor', {
         title: 'About'

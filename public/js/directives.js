16ce177cece8b55f62289cc70307433a0baa3723
chiller <egalaczi_business@yahoo.com>
2013-09-27 19:37:30 +0200

    remove onkeyupfn

--- public/js/directives.js
+++ public/js/directives.js
@@ -77,20 +77,4 @@ ntmodule.directive('saveSelected', function(EntityService){
     };  
 })
 
-ntmodule.directive('onKeyupFn', function() {
-    return function(scope, elm, attrs) {
-        //Evaluate the variable that was passed
-        //In this case we're just passing a variable that points
-        //to a function we'll call each keyup
-        var keyupFn = scope.$eval(attrs.onKeyupFn);
-        angular.element(document).bind('keyup', function(evt) {
-            //$apply makes sure that angular knows 
-            //we're changing something
-            scope.$apply(function() {
-                keyupFn.call(scope, evt.which);
-            });
-
-        });
-    };
-});
 

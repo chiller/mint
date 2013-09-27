981d3a74f289f1775969305ffda7722da33885bf
chiller <egalaczi_business@yahoo.com>
2013-09-27 19:54:29 +0200

    connection changes are also only sent to room

--- routes/api/connections.js
+++ routes/api/connections.js
@@ -8,7 +8,7 @@ module.exports = function (db, sa) {
     this.create = function(req,res){
         var oid = mongo.BSONPure.ObjectID(req.params.id)
         docs.update({_id:oid},{ $push: {connections: {from:req.body.from, to: req.body.to}}}  ,function(obj){
-           sa.broadcast("room","connection:create",{"msg":"created", obj:{from:req.body.from, to: req.body.to}});
+           sa.broadcast(req.params.id,"connection:create",{"msg":"created", obj:{from:req.body.from, to: req.body.to}});
            res.json({"msg":"created", obj:{from:req.body.from, to: req.body.to}});
         })
 
@@ -19,7 +19,7 @@ module.exports = function (db, sa) {
         var url_parts = url.parse(req.url, true);
         var oid = mongo.BSONPure.ObjectID(url_parts.query._id)
         docs.update({_id:oid},{ $pull: {connections: {from:url_parts.query.from, to: url_parts.query.to}}}  ,function(obj){
-            sa.broadcast("room","connection:delete", {msg:"deleted",obj: {from:url_parts.query.from, to: url_parts.query.to}});
+            sa.broadcast(url_parts.query.id,"connection:delete", {msg:"deleted",obj: {from:url_parts.query.from, to: url_parts.query.to}});
             res.json({msg:"deleted",obj: {from:url_parts.query.from, to: url_parts.query.to}}) ;
         })
 

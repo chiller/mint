9716adf91aca688c91ae0e5e9c70d4e5c5d5f790
chiller <egalaczi_business@yahoo.com>
2013-11-05 14:19:43 +0100

    prod and dev db settings

--- app.js
+++ app.js
@@ -12,7 +12,18 @@ var app = module.exports = express.createServer();
 // Configuration
 var conn_string_prod = "mongodb://nodejitsu:54994e0b6fa6817af58f0160756facf7@paulo.mongohq.com:10060/nodejitsudb6288570733";
 var conn_string_local = "mongodb://localhost:27017/mint"
-MongoClient.connect(conn_string_local, function(err, db) {
+
+app.configure('development', function(){
+    conn_string = conn_string_local
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+    conn_string = conn_string_prod
+    app.use(express.errorHandler());
+});
+
+MongoClient.connect(conn_string, function(err, db) {
     "use strict";
     if(err) throw err;
 
@@ -28,14 +39,6 @@ MongoClient.connect(conn_string_local, function(err, db) {
       app.use(express.static(__dirname + '/public'));
       app.use(express.logger());
       app.use(app.router);
-    });
-
-    app.configure('development', function(){
-      app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-    });
-
-    app.configure('production', function(){
-      app.use(express.errorHandler());
     });
 
     routes(app, db);
